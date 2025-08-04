053c2f1a6158815ec7947116c39f2517205f7470
thakorajit178 <ajit.thakor@cybersurfindia.com>
2025-08-04 13:52:58 +0530

    add: player changes

---
+++ providers/4khdhub/catalog.ts
@@ -0,0 +1,20 @@
+export const catalog = [
+  {
+    title: "Popular Movies",
+    filter: "/category/new-movies-10810.html",
+  },
+  {
+    title: "Latest TV Shows",
+    filter: "/category/new-series-10811.html",
+  },
+  {
+    title: "Anime",
+    filter: "/category/anime-10812.html",
+  },
+  {
+    title: "4K HDR",
+    filter: "/category/4k-hdr-10776.html",
+  },
+];
+
+export const genres = [];
---
+++ providers/movieBox/catalog.ts
@@ -0,0 +1,12 @@
+export const catalog = [
+  {
+    title: "Trending",
+    filter: "2",
+  },
+  {
+    title: "Cinema",
+    filter: "5",
+  },
+];
+
+export const genres = [];
